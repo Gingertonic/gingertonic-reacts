117bb60e2ef1d3109e4555c55561f917cd700ef3
Gingertonic <bethmschofield@gmail.com>
2020-08-17 23:17:04 +0100

    tidy react-router-dom import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './styles/normalize.css';
 import './styles/index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>,
+    </Router>,
     document.getElementById('root')
 );
